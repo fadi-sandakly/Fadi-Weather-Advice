2a560388aabd810fbfff80ca0dacc6e075e6eb2f
Bamoe <bamoe@ibm.com>
2023-04-12 05:29:25 -0700

    Changed rule message

--- target/generated-sources/kogito/org/kie/kogito/app/HotReloadSupportClass.java
+++ target/generated-sources/kogito/org/kie/kogito/app/HotReloadSupportClass.java
@@ -1,5 +1,5 @@
 package org.kie.kogito.app;
 @io.quarkus.runtime.Startup()
 public class HotReloadSupportClass {
-private static final String ID = "671951c7-40e6-4954-9fe1-3132950145c0";
+private static final String ID = "0ab6100b-7c7b-42ff-9f8a-4d4d93eb6da6";
 }
