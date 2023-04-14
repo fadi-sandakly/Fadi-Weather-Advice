e426535a3ee214479a8d158705734d910d9f5a40
Bamoe <bamoe@ibm.com>
2023-04-14 01:14:12 -0700

    Another First commit

--- target/generated-sources/kogito/org/kie/kogito/app/HotReloadSupportClass.java
+++ target/generated-sources/kogito/org/kie/kogito/app/HotReloadSupportClass.java
@@ -1,5 +1,5 @@
 package org.kie.kogito.app;
 @io.quarkus.runtime.Startup()
 public class HotReloadSupportClass {
-private static final String ID = "d06e3ec4-d791-4ddf-9ae9-7e9e7a5d71d4";
+private static final String ID = "39d5b4c7-65c1-46b8-86df-c15d5e96cf96";
 }
